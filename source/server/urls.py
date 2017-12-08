2ab30c065d0df87376efe4a263743300c965b662
Fernando Espíndola <fernando.espindola@hp.com>
2017-12-08 18:17:29 -0200

    WIP

--- source/server/urls.py
+++ source/server/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 from catv.views import VideoListView, PlaylistListView, VideoDetailView, TagsView
 
 urlpatterns = [
@@ -12,4 +12,7 @@ urlpatterns = [
     # url(r'^videos/(?P<pk>.*)$', VideoDetailView.as_view(), name='video-detail'),
     url(r'^videos/(?P<slug>[-\w]+)/$', VideoDetailView.as_view(), name='video-detail'),
     url(r'^tags$', TagsView.as_view(), name='tags'),
+
+    # ClassRomm
+    url(r'^classroom/$', TemplateView.as_view(template_name='classroom/index.html')),
 ]
